ad0a813ca1653b08878e149797c287dc919f6f4c
yangjc <bupt_yjc@163.com>
2018-12-30 16:06:44 +0800

    change add_pos

--- nn_arch.py
+++ nn_arch.py
@@ -10,7 +10,7 @@ class AttEncode(nn.Module):
         super(AttEncode, self).__init__()
         self.stack, self.head = stack, head
         en_vocab_num, en_embed_len = en_embed_mat.size()
-        self.en_embed_len = en_embed_len
+        self.embed_len = en_embed_len
         self.en_embed = nn.Embedding(en_vocab_num, en_embed_len, _weight=en_embed_mat)
         self.querys, self.keys, self.vals = [[[nn.Linear(en_embed_len, 200)] * head] * stack] * 3
         self.fuses = [nn.Linear(200 * head, 200)] * stack
@@ -32,13 +32,15 @@ class AttEncode(nn.Module):
         return x
 
     def make_pos(self, x):
-        p = torch.zeros(x.size(0), x.size(1), self.en_embed_len)
+        p = torch.zeros(x.size(0), x.size(1), self.embed_len)
         for i in range(x.size(0)):
-            z = (x[i] == 0).sum().byte().item()
-            for j in range(x.size(1) - z):
-
-                for k in range(self.en_embed_len):
-                    pass
+            off = (x[i] == 0).sum().byte().item()
+            for j in range(x.size(1) - off):
+                for k in range(self.embed_len):
+                    if k % 2:
+                        p[i, j + off, k] = torch.sin(j / torch.pow(1e3, j / self.embed_len))
+                    else:
+                        p[i, j + off, k] = torch.cos(j / torch.pow(1e3, j / self.embed_len))
         return p
 
     def mul_att(self, x, i):
@@ -59,7 +61,7 @@ class AttDecode(nn.Module):
         super(AttDecode, self).__init__()
         self.stack, self.head = stack, head
         zh_vocab_num, zh_embed_len = zh_embed_mat.size()
-        self.zh_embed_len = zh_embed_len
+        self.embed_len = zh_embed_len
         self.zh_embed = nn.Embedding(zh_vocab_num, zh_embed_len, _weight=zh_embed_mat)
         self.querys, self.keys, self.vals = [[[nn.Linear(zh_embed_len, 200)] * head] * stack] * 3
         self.fuses = [nn.Linear(200 * head, 200)] * stack
@@ -81,7 +83,15 @@ class AttDecode(nn.Module):
         return x
 
     def make_pos(self, x):
-        p = torch.zeros(x.size(0), x.size(1), self.en_embed_len)
+        p = torch.zeros(x.size(0), x.size(1), self.embed_len)
+        for i in range(x.size(0)):
+            off = (x[i] == 0).sum().byte().item()
+            for j in range(x.size(1) - off):
+                for k in range(self.embed_len):
+                    if k % 2:
+                        p[i, j + off, k] = torch.sin(j / torch.pow(1e3, j / self.embed_len))
+                    else:
+                        p[i, j + off, k] = torch.cos(j / torch.pow(1e3, j / self.embed_len))
         return p
 
     def mul_att(self, x, i):
