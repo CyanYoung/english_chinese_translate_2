eb48e32fc75a8928c734f1f2c10adc8b7aadb09b
yangjc <bupt_yjc@163.com>
2018-12-30 16:32:36 +0800

    change make_pos

--- nn_arch.py
+++ nn_arch.py
@@ -24,7 +24,7 @@ class AttEncode(nn.Module):
         x = x + p
         for i in range(self.stack):
             r = x
-            x = self.mul_att(x, i)
+            x = self.mul_att(x, x, i)
             x = F.layer_norm(x + r, x.size()[1:])
             r = x
             x = self.lals[i](x)
@@ -33,20 +33,20 @@ class AttEncode(nn.Module):
 
     def make_pos(self, x):
         p = torch.zeros(x.size(0), x.size(1), self.embed_len)
-        for i in range(x.size(0)):
-            off = (x[i] == 0).sum().byte().item()
-            for j in range(x.size(1) - off):
-                for k in range(self.embed_len):
-                    if k % 2:
-                        p[i, j + off, k] = torch.sin(j / torch.pow(1e3, j / self.embed_len))
+        for k in range(x.size(0)):
+            off = (x[k] == 0).sum().byte().item()
+            for i in range(x.size(1) - off):
+                for j in range(self.embed_len):
+                    if j % 2:
+                        p[k, i + off, j] = torch.sin(i / torch.pow(1e3, i / self.embed_len))
                     else:
-                        p[i, j + off, k] = torch.cos(j / torch.pow(1e3, j / self.embed_len))
+                        p[k, i + off, j] = torch.cos(i / torch.pow(1e3, i / self.embed_len))
         return p
 
-    def mul_att(self, x, i):
+    def mul_att(self, h1, h2, i):
         c = list()
         for j in range(self.head):
-            q, k, v = self.querys[i][j](x), self.keys[i][j](x), self.vals[i][j](x)
+            q, k, v = self.querys[i][j](h2), self.keys[i][j](h1), self.vals[i][j](h1)
             scale = math.sqrt(k.size(-1))
             d = torch.matmul(q, k.permute(0, 2, 1)) / scale
             a = F.softmax(d, dim=-1)
@@ -63,19 +63,19 @@ class AttDecode(nn.Module):
         zh_vocab_num, zh_embed_len = zh_embed_mat.size()
         self.embed_len = zh_embed_len
         self.zh_embed = nn.Embedding(zh_vocab_num, zh_embed_len, _weight=zh_embed_mat)
-        self.querys, self.keys, self.vals = [[[nn.Linear(zh_embed_len, 200)] * head] * stack] * 3
+        self.querys, self.keys, self.vals = [[[[nn.Linear(zh_embed_len, 200)] * head] * stack] * 2] * 3
         self.fuses = [nn.Linear(200 * head, 200)] * stack
         self.lals = [nn.Sequential(nn.Linear(200, 200),
                                    nn.ReLU(),
                                    nn.Linear(200, 200))] * stack
 
-    def forward(self, x):
-        p = self.make_pos(x)
-        x = self.zh_embed(x)
-        x = x + p
+    def forward(self, y, x):
+        p = self.make_pos(y)
+        y = self.zh_embed(y)
+        y = y + p
         for i in range(self.stack):
-            r = x
-            x = self.mul_att(x, i)
+            r = y
+            x = self.mul_att(y, y, i)
             x = F.layer_norm(x + r, x.size()[1:])
             r = x
             x = self.lals[i](x)
@@ -84,20 +84,20 @@ class AttDecode(nn.Module):
 
     def make_pos(self, x):
         p = torch.zeros(x.size(0), x.size(1), self.embed_len)
-        for i in range(x.size(0)):
-            off = (x[i] == 0).sum().byte().item()
-            for j in range(x.size(1) - off):
-                for k in range(self.embed_len):
-                    if k % 2:
-                        p[i, j + off, k] = torch.sin(j / torch.pow(1e3, j / self.embed_len))
+        for k in range(x.size(0)):
+            off = (x[k] == 0).sum().byte().item()
+            for i in range(x.size(1) - off):
+                for j in range(self.embed_len):
+                    if j % 2:
+                        p[k, i + off, j] = torch.sin(i / torch.pow(1e3, i / self.embed_len))
                     else:
-                        p[i, j + off, k] = torch.cos(j / torch.pow(1e3, j / self.embed_len))
+                        p[k, i + off, j] = torch.cos(i / torch.pow(1e3, i / self.embed_len))
         return p
 
-    def mul_att(self, x, i):
+    def mul_att(self, h1, h2, i):
         c = list()
         for j in range(self.head):
-            q, k, v = self.querys[i][j](x), self.keys[i][j](x), self.vals[i][j](x)
+            q, k, v = self.querys[i][j](h2), self.keys[i][j](h1), self.vals[i][j](h1)
             scale = math.sqrt(k.size(-1))
             d = torch.matmul(q, k.permute(0, 2, 1)) / scale
             a = F.softmax(d, dim=-1)
