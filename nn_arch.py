bb3f2d53fe27e81cfc2865955fdd55ee9a4276fa
yangjc <bupt_yjc@163.com>
2019-01-22 11:17:02 +0800

    add mask

--- nn_arch.py
+++ nn_arch.py
@@ -6,10 +6,10 @@ import torch.nn.functional as F
 
 
 class Trm(nn.Module):
-    def __init__(self, en_embed_mat, zh_embed_mat, pos_mat, head, stack):
+    def __init__(self, en_embed_mat, zh_embed_mat, pos_mat, mask_mat, head, stack):
         super(Trm, self).__init__()
         self.encode = TrmEncode(en_embed_mat, pos_mat, head, stack)
-        self.decode = TrmDecode(zh_embed_mat, pos_mat, head, stack)
+        self.decode = TrmDecode(zh_embed_mat, pos_mat, mask_mat, head, stack)
 
     def forward(self, x, y):
         h = self.encode(x)
@@ -65,21 +65,21 @@ class EncodeLayer(nn.Module):
 
 
 class TrmDecode(nn.Module):
-    def __init__(self, zh_embed_mat, pos_mat, head, stack):
+    def __init__(self, zh_embed_mat, pos_mat, mask_mat, head, stack):
         super(TrmDecode, self).__init__()
         zh_vocab_num, zh_embed_len = zh_embed_mat.size()
         self.zh_embed = nn.Embedding(zh_vocab_num, zh_embed_len, _weight=zh_embed_mat)
-        self.pos = pos_mat
+        self.pos, self.mask = pos_mat, mask_mat
         self.layers = nn.ModuleList([DecodeLayer(zh_embed_len, head) for _ in range(stack)])
         self.dl = nn.Sequential(nn.Dropout(0.2),
                                 nn.Linear(200, zh_vocab_num))
 
     def forward(self, y, h):
-        p = self.pos.repeat(y.size(0), 1, 1)
+        p, m = self.pos.repeat(y.size(0), 1, 1), self.mask.repeat(y.size(0), 1, 1, 1)
         y = self.zh_embed(y)
         y = y + p
         for layer in self.layers:
-            y = layer(y, h)
+            y = layer(y, h, m)
         return self.dl(y)
 
 
@@ -95,22 +95,24 @@ class DecodeLayer(nn.Module):
                                  nn.ReLU(),
                                  nn.Linear(200, 200))
 
-    def mul_att(self, x, y, i):
+    def mul_att(self, x, y, m, i):
         q = self.qrys[i](y).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
         k = self.keys[i](x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
         v = self.vals[i](x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
         d = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(k.size(-1))
+        if i == 0:
+            d = d.masked_fill(m, -1e9)
         a = F.softmax(d, dim=-1)
         c = torch.matmul(a, v).transpose(1, 2)
         c = c.contiguous().view(c.size(0), c.size(1), -1)
         return self.fuse(c)
 
-    def forward(self, y, h):
+    def forward(self, y, h, m):
         r = y
-        y = self.mul_att(y, y, 0)
+        y = self.mul_att(y, y, m, 0)
         y = F.layer_norm(y + r, y.size()[1:])
         r = y
-        y = self.mul_att(h, y, 1)
+        y = self.mul_att(h, y, m, 1)
         y = F.layer_norm(y + r, y.size()[1:])
         r = y
         y = self.lal(y)
