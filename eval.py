4a126b7da16c2f3c51c8168d7e57abe773e4d41e
yangjc <bupt_yjc@163.com>
2019-01-26 18:16:08 +0800

    change struct

--- eval.py
+++ eval.py
@@ -13,6 +13,8 @@ from util import map_item
 
 device = torch.device('cpu')
 
+get_text, get_bleu = True, False
+
 path_dev_en_sent = 'feat/en_sent_dev.pkl'
 path_dev_zh_sent = 'feat/zh_sent_dev.pkl'
 path_dev_label = 'feat/label_dev.pkl'
@@ -33,22 +35,23 @@ with open(path_test_label, 'rb') as f:
     test_labels = pk.load(f)
 
 
-def pair_print(prod_mat, label_mat, ind_words):
+def pair_print(label_mat, prod_mat, ind_words, mode):
     pred_mat = torch.max(prod_mat, dim=-1)[1]
+    print('\n%s: \n' % mode)
     for preds, labels in zip(pred_mat, label_mat):
-        pairs = list()
-        for pred, label in zip(preds, labels):
-            pairs.append(' -> '.join([ind_words[pred], ind_words[label]]))
-        print(', '.join(pairs))
+        bound = (labels > 0).sum().item()
+        label = ''.join([ind_words[label.item()] for label in labels[:bound]])
+        pred = ''.join([ind_words[pred.item()] for pred in preds[:bound]])
+        print(' | '.join([label, pred]))
 
 
-def check(name, triples, cand):
+def debug(name, triples, cand):
     en_sents, zh_sents, labels = tensorize(triples, device)
     model = map_item(name, models)
     with torch.no_grad():
         model.eval()
         prods = model(en_sents[:cand], zh_sents[:cand])
-    pair_print(prods, labels, zh_ind_words)
+    pair_print(labels, prods, zh_ind_words, 'trm')
     encode = map_item(name + '_encode', models)
     decode = map_item(name + '_decode', models)
     with torch.no_grad():
@@ -56,12 +59,10 @@ def check(name, triples, cand):
         states = encode(en_sents[:cand])
         decode.eval()
         prods = decode(zh_sents[:cand], states)
-    pair_print(prods, labels, zh_ind_words)
+    pair_print(labels, prods, zh_ind_words, 'trm_sep')
 
 
-def test(name, dev_triples, test_en_sents, test_labels, debug):
-    if debug:
-        check(name, dev_triples, cand=10)
+def test(name, test_en_sents, test_labels):
     labels = [[label.split()] for label in test_labels]
     preds = list()
     for en_sent in test_en_sents:
@@ -71,4 +72,7 @@ def test(name, dev_triples, test_en_sents, test_labels, debug):
 
 
 if __name__ == '__main__':
-    test('trm', dev_triples, test_en_sents, test_labels, debug=True)
+    if get_text:
+        debug('trm', dev_triples, cand=10)
+    if get_bleu:
+        test('trm', test_en_sents, test_labels)
